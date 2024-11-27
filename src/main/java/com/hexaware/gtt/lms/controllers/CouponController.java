dad9ca4c0f1a6a479e8aab02563accd694eb1e71
2000137407 <2000137407@LTCH-98R24253GP.corp.hexaware.com>
2024-11-27 15:09:20 +0530

    User Coupon Services

---
+++ src/main/java/com/hexaware/gtt/lms/controllers/CouponController.java
@@ -0,0 +1,11 @@
+package com.hexaware.gtt.lms.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CouponController {
+
+	@Autowired
+	private ModelMapper modelMapper;
+	
+}
